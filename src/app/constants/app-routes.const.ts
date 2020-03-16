e66461bb73f5f6832a9f10655e6d7bdfe64529b8
Suman Jha <suman.jha@fiserv.com>
2020-03-15 20:03:23 -0700

    updating nav

--- src/app/constants/app-routes.const.ts
+++ src/app/constants/app-routes.const.ts
@@ -2,38 +2,47 @@ import { RouteLinkModel } from '../models/route-link.model';
 
 export const APP_ROUTES: RouteLinkModel[] = [
   {
+    iconName: 'settings',
     displayName: 'Printer Setting',
     route: 'setting'
   }, {
+    iconName: 'print',
     displayName: 'Print Options',
     route: 'printOptions'
   }, {
     displayName: 'Canvas Print',
     route: 'canvas',
+    iconName: 'style',
     children: [
       {
         displayName: 'Image',
         route: 'canvas/image',
+        iconName: 'image'
       },
       {
         displayName: 'Text',
         route: 'canvas/text',
+        iconName: 'text_format'
       },
       {
         displayName: 'Bar Code',
         route: 'canvas/barcode',
+        iconName: 'bar_chart'
       },
       {
         displayName: 'Graph',
         route: 'canvas/graph',
+        iconName: 'graphic_eq'
       },
       {
         displayName: 'Handwriting',
-        route: 'canvas/handwriting'
+        route: 'canvas/handwriting',
+        iconName: 'format_paint'
       },
       {
         displayName: 'Label',
-        route: 'canvas/label'
+        route: 'canvas/label',
+        iconName: 'label'
       }
     ]
   }
